0a8bb2fe6b7573081ee2b0d046e0a45ab167dba6
CamillaSöderman <camilla.e.sodermna@gmail.com>
2022-12-20 11:21:08 +0100

    first try

---
+++ src/test/java/com/example/WishlistSanta/WishlistSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WishlistSanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
